5d777af492675dfdb177ed09b133cbff6a0b7f2a
Mazur Denis <kilerden2002@gmail.com>
2024-12-13 17:33:26 +1000

    Update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Auto():
+    id = models.PositiveIntegerField()
+    model = models.CharField('Модель', max_length=50)
+    year = models.PositiveIntegerField()
+    miliege = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    power_volume = models.CharField('Модель', max_length=5)
+    transmission = models.CharField(max_length=50)
+    engine_volume = models.CharField(max_length=50)
+    drive = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.model
